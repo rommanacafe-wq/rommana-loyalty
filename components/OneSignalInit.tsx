ec6b784c8a2b1b57baaf1ffdad87c2d2c65c11bf
Leen Yaghi <rommana.cafe@gmail.com>
2026-04-16 23:55:09 -0400

    Move OneSignal worker

--- components/OneSignalInit.tsx
+++ components/OneSignalInit.tsx
@@ -24,14 +24,12 @@ export default function OneSignalInit({ externalId }: Props) {
       window.__onesignalInitialized = true
 
       await OneSignal.init({
-  appId: "f82ff25f-3ee6-4aee-a160-fc3cf4b419a4",
-
-  serviceWorkerPath: '/OneSignalSDKWorker.js',
-  serviceWorkerUpdaterPath: '/OneSignalSDKWorker.js',
-  serviceWorkerParam: { scope: '/' },
-
-  notifyButton: { enable: true },
-})
+        appId: 'f82ff25f-3ee6-4aee-a160-fc3cf4b419a4',
+        safari_web_id: 'web.onesignal.auto.37a4bd23-e633-4ae3-9e22-29e91fb790d4',
+        notifyButton: { enable: false },
+        serviceWorkerPath: 'push/onesignal/OneSignalSDKWorker.js',
+        serviceWorkerParam: { scope: '/push/onesignal/' },
+      })
 
       if (externalId) {
         await OneSignal.login(externalId)
