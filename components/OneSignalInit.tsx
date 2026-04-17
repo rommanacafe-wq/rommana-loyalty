b317a0e08a1742f99dc4110d07008b0827431190
Leen Yaghi <rommana.cafe@gmail.com>
2026-04-16 23:37:49 -0400

    Update OneSignal

--- components/OneSignalInit.tsx
+++ components/OneSignalInit.tsx
@@ -24,15 +24,17 @@ export default function OneSignalInit({ externalId }: Props) {
       window.__onesignalInitialized = true
 
       await OneSignal.init({
-        appId: 'f82ff25f-3ee6-4aee-a160-fc3cf4b419a4',
-        safari_web_id: 'web.onesignal.auto.37a4bd23-e633-4ae3-9e22-29e91fb790d4',
-        notifyButton: { enable: false },
-        serviceWorkerPath: '/OneSignalSDKWorker.js',
-      })
+  appId: "f82ff25f-3ee6-4aee-a160-fc3cf4b419a4",
+
+  serviceWorkerPath: '/OneSignalSDKWorker.js',
+  serviceWorkerUpdaterPath: '/OneSignalSDKWorker.js',
+  serviceWorkerParam: { scope: '/' },
+
+  notifyButton: { enable: true },
+})
 
       if (externalId) {
         await OneSignal.login(externalId)
-        console.log('OneSignal logged in:', externalId)
       }
 
       console.log('OneSignal initialized')
