d1d1a1ece1b13319c36848edd45018e096e1e479
rommanacafe-wq <rommana.cafe@gmail.com>
2026-04-25 15:11:19 -0400

    Change Supabase client to use hardcoded values
    Updated Supabase client to use hardcoded URL and anon key.

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -2,8 +2,8 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.https://xisiuptrpbgblxinameo.supabase.co/rest/v1/!,
+    process.env.NEXT_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhpc2l1cHRycGJnYmx4aW5hbWVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1MTAzODUsImV4cCI6MjA5MTA4NjM4NX0.PX1PuvzcCOqX54q99Dp23BuTjXp296wYKeKJQmg5KJY!,
     {
       auth: {
         persistSession: true,
